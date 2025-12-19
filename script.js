f2ca29f32a0d4710a4121c3908e00087b8abc973
Andrew Marcy <mar70823@pennwest.edu>
2025-12-18 19:41:15 -0500

    Linked the phone number and name together. Also Displays it on screen to test that it worked.

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const button = document.getElementById("myButton");
 const message = document.getElementById("message");
 const submit = document.getElementById("submitButton")
-const input = document.getElementById("textInput");
+const nameInput = document.getElementById("textInput");
 const phoneInput = document.getElementById("phoneInput");
 
 
 
 
-let enteredText = "";
+const phoneBookData = [];
 
-submit.addEventListener("click", () => {
-    enteredText = input.value;
 
-    message.textContent = enteredText;
-});
+submit.addEventListener("click", submitEntry);
+phoneInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        submitEntry();
+    }
+} )
+
+
+function submitEntry() {
+        const name = nameInput.value;
+    const phoneNumber = phoneInput.value;
+
+    if(name === "" || phoneNumber === "") {
+        alert("Please enter name and phone number");
+        return;
+    }
+
+    const entry = {name: name, phone: phoneNumber};
+    phoneBookData.push(entry);
+
+    //message.textContent = "HI"
+    message.textContent = phoneBookData.map(e => `${e.name}: ${e.phone}`).join("\n");
+
+    nameInput.value = "";
+    phoneInput.value = "";
+
+}
